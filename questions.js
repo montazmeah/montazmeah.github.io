d1148ae855cd9a503fae370550938f9a7ef558f9
Montaz Meah <montazmeahii@yahoo.com>
2024-02-26 10:34:00 -0500

    Added notes for question 9

--- questions.js
+++ questions.js
@@ -235,7 +235,75 @@ let questions = [
         text: "Use AdBox URLs",
       },
     ],
-    notes: ``,
+    notes: `
+    <p>source: <a target="_blank" href="https://experienceleague.adobe.com/docs/target-dev/developer/implement-email/testing-email-image-adbox.html?lang=en">https://experienceleague.adobe.com/docs/target-dev/developer/implement-email/testing-email-image-adbox.html?lang=en</a></p>
+    <div class="docs-main">
+  <!-- Docs page metadata-->
+  <h4 id="test-an-email-image-adbox" tabindex="-1">Test an email image Adbox</h4>
+
+  
+  <p>Dynamically test images in email, and even change those images on the fly when someone opens the email.</p>
+<p>Redirectors can be used in emails to track clicks and dynamically control which landing page people reach.</p>
+<p>Email image testing is achieved through using modified versions of adboxes. Because email clients do not allow cookies to be set, a unique identifier must be generated for each email. This number is appended to the adbox URL and to any redirectors used in the email to track clicks from the email.</p>
+<div class="extension note">
+<div>NOTE</div>
+<div>
+<p>Although Target can technically deliver image optimization at open time, each email client handles caching differently, so success will be varied. Regardless of the email service provider used, the email client that the end user uses to open the email (Gmail app, Outlook, Hotmail, and so forth) determines whether the image is actually fetched at open-time. For example, Gmail caches most everything, so the image the end user sees is dependent on when Gmail chooses to call and cache the image.</p>
+</div>
+</div>
+<p><strong>Sample code for an email image adbox:</strong></p>
+<div class="code-toolbar"><code class="language-none">&lt;img src="https://{clientcode}.tt.omtrdc.net/m2/​{clientcode}/ubox/​image?
+mbox={email_header}&amp;
+mboxDefault=​{http%3A%2F%2Fwww.domain.com%2Fheader.jpg}&amp;
+mboxXDomain=disabled&amp;
+mboxSession={123456}&amp;
+mboxPC={123456}" border=:"0"/&gt;
+</code>
+</div>
+<p>Where the below values are specific to you:</p>
+<div class="table-container">
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Value</th>
+      <th scope="col">Description</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">clientcode</th>
+      <td>Your company’s client code. Find this in your at.js listed as <code>clientCode='yourclientcode'</code>. This is all lower case and has no special characters.</td>
+    </tr>
+    <tr>
+      <th scope="row">image</th>
+      <td>The offer type. It is always “image” for graphic ads and “page” for redirectors.</td>
+    </tr>
+    <tr>
+      <th scope="row">email_header</th>
+      <td>The name of the adbox.</td>
+    </tr>
+    <tr>
+      <th scope="row"><code>mboxDefault=http%3A%2F%2Fwww.domain.com%2Fheader.jpg	</code</th>
+      <td>Required. Replace the URL with appropriate default content for your adbox. This must be an absolute reference and must be URL encoded.</td>
+    </tr>
+    <tr>
+      <th scope="row"><code>mboxXDomain=disabled</code</th>
+      <td>Tells Target to not attempt to set a cookie.</td>
+    </tr>
+    <tr>
+      <th scope="row"><code>mboxSession=123456</code> and <code>mboxPC=123456</code</th>
+      <td>Two values required by Target to merge this user’s profile with the existing profile for your site. 123456 is the unique identifier generated per email. Dynamically insert this value into every adbox and redirector URL. This number must be unique for each email sent to each person. If a weekly email is sent to 1,000 people, 1,000 unique IDs need to be generated.
+      The unique identifier per email needs to be assigned to the mboxSession and mboxPC in each adbox and redirector URL. The recommended format for this identifier is timestamp-NNNNN where NNNNN is a random 5-digit number, but any alphanumeric format will work. Some mass e-mail services and any programming language are capable of generating this unique identifier.
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
+
+</div>`,
   },
   {
     number: 10,
